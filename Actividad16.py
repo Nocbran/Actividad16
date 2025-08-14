02fc2d8885118a46d0cb20b0527f313cbd84aca9
Brand <vasbran111@gmail.com>
2025-08-13 19:37:01 -0500

    .

--- Actividad16.py
+++ Actividad16.py
@@ -1,9 +1,19 @@
 class RegistoLibros:
-    def __init__(self,titulo,autor,añopublicado,codigounico):
-        self.titulo = titulo
-        self.añopublicado = añopublicado
+    def __init__(self,codigounico,titulo,autor,añopublicado,):
         self.codigounico = codigounico
+        self.titulo = titulo
+        self.autor = autor
+        self.añopublicado = añopublicado
+
 
         def MostrarInfo(self):
-            return f"Codigo Unico: {self.codigounico} - Titulo: {self.titulo} - Año Publicado: {self.añopublicado} "
+            return f"Codigo Unico: {self.codigounico} - Titulo: {self.titulo} - Autor: {self.autor} - Año Publicado: {self.añopublicado}"
 class Usuarios:
+    def __init__(self,carnet,nombre,carrera):
+        self.carnet = carnet
+        self.nombre = nombre
+        self.carrera = carrera
+
+        def MostarInf(self):
+            return f"Carnet: {self.carnet} - Nombre: {self.nombre} - Carrera {self.carrera}"
+
