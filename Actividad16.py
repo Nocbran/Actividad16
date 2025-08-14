f857ebdbd59d463a1ebc19167a29d1b11c43049e
Brand <vasbran111@gmail.com>
2025-08-13 19:24:19 -0500

    .

---
+++ Actividad16.py
@@ -0,0 +1,9 @@
+class RegistoLibros:
+    def __init__(self,titulo,autor,añopublicado,codigounico):
+        self.titulo = titulo
+        self.añopublicado = añopublicado
+        self.codigounico = codigounico
+
+        def MostrarInfo(self):
+            return f"Codigo Unico: {self.codigounico} - Titulo: {self.titulo} - Año Publicado: {self.añopublicado} "
+class Usuarios:
